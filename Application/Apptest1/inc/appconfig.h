94b2b6c838561d862c7547313de316b0301c8802
Arnaud <arnaud.chanu@gmail.com>
2016-10-27 22:39:39 -0400

    Initial commit

---
+++ Application/Apptest1/inc/appconfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for Test App
+#define APPTEST1_VERSION_MAJOR 1
+#define APPTEST1_VERSION_MINOR 0
---
+++ Application/Apptest1/inc/apptest1.h
@@ -0,0 +1,21 @@
+#ifndef APPTEST1_H_   
+#define APPTEST1_H_
+//Includes
+
+#include "types.h"
+//Defines
+
+//Structures
+typedef struct sBuilding
+{
+	TUInt8 b1NbFloor;
+	TUInt8 b1NbElevator;
+	TUChar *name;
+
+}tBuilding;
+//Enums
+
+//Prototypes
+
+
+#endif // TESTAPP_H_
